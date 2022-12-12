f0a4e8e9f7c3bcf90abdfb1f213ad0d4b71c0c9c
chris3evans <chris3evans.ce@gmail.com>
2022-12-12 16:55:09 +0000

    Styled skill lists

--- src/Components/SkillLists/BackEndSkillList.js
+++ src/Components/SkillLists/BackEndSkillList.js
@@ -4,17 +4,31 @@ import SkillItem from "../SkillItem/SkillItem";
 
 const BackEndSkillList = function () {
   return (
-    <div className={styles["skill-list"]}>
-      <h3 className={styles["skill-list-heading"]}>Back End</h3>
-      <ul className={styles["skills"]}>
-        <SkillItem skill="Node"></SkillItem>
-        <SkillItem skill="Express"></SkillItem>
-        <SkillItem skill="Koa"></SkillItem>
-        <SkillItem skill="PostgreSQL"></SkillItem>
-        <SkillItem skill="Sequelize"></SkillItem>
-        <SkillItem skill="MongoDB"></SkillItem>
-        <SkillItem skill="Mongoose"></SkillItem>
-      </ul>
+    <div className={styles["skills"]}>
+      <div className={styles["skills-top-light-container"]}>
+        <img
+          className={styles["skills-top-light-image"]}
+          src={require("../../assets/top-spot-light-transparent.png")}
+        ></img>
+      </div>
+      <div className={styles["skills-container"]}>
+        <h3 className={styles["skill-list-heading"]}>Back End</h3>
+        <ul className={styles["skills-list"]}>
+          <SkillItem skill="Node"></SkillItem>
+          <SkillItem skill="Express"></SkillItem>
+          <SkillItem skill="Koa"></SkillItem>
+          <SkillItem skill="PostgreSQL"></SkillItem>
+          <SkillItem skill="Sequelize"></SkillItem>
+          <SkillItem skill="MongoDB"></SkillItem>
+          <SkillItem skill="Mongoose"></SkillItem>
+        </ul>
+      </div>
+      <div className={styles["skills-bottom-light-container"]}>
+        <img
+          className={styles["skills-bottom-light-image"]}
+          src={require("../../assets/bottom-spot-light-transparent.png")}
+        ></img>
+      </div>
     </div>
   );
 };
--- src/Components/SkillLists/OtherSkillList.js
+++ src/Components/SkillLists/OtherSkillList.js
@@ -4,17 +4,31 @@ import SkillItem from "../SkillItem/SkillItem";
 
 const OtherSkillList = function () {
   return (
-    <div className={styles["skill-list"]}>
-      <h3 className={styles["skill-list-heading"]}>Other</h3>
-      <ul className={styles["skills"]}>
-        <SkillItem skill="TypeScript"></SkillItem>
-        <SkillItem skill="GitHub"></SkillItem>
-        <SkillItem skill="GitFlow"></SkillItem>
-        <SkillItem skill="Responsive Design"></SkillItem>
-        <SkillItem skill="Material UI"></SkillItem>
-        <SkillItem skill="Cypress"></SkillItem>
-        <SkillItem skill="Jest"></SkillItem>
-      </ul>
+    <div className={styles["skills"]}>
+      <div className={styles["skills-top-light-container"]}>
+        <img
+          className={styles["skills-top-light-image"]}
+          src={require("../../assets/top-spot-light-transparent.png")}
+        ></img>
+      </div>
+      <div className={styles["skills-container"]}>
+        <h3 className={styles["skill-list-heading"]}>Other</h3>
+        <ul className={styles["skills-list"]}>
+          <SkillItem skill="TypeScript"></SkillItem>
+          <SkillItem skill="GitHub"></SkillItem>
+          <SkillItem skill="GitFlow"></SkillItem>
+          <SkillItem skill="Responsive Design"></SkillItem>
+          <SkillItem skill="Material UI"></SkillItem>
+          <SkillItem skill="Cypress"></SkillItem>
+          <SkillItem skill="Jest"></SkillItem>
+        </ul>
+      </div>
+      <div className={styles["skills-bottom-light-container"]}>
+        <img
+          className={styles["skills-bottom-light-image"]}
+          src={require("../../assets/bottom-spot-light-transparent.png")}
+        ></img>
+      </div>
     </div>
   );
 };

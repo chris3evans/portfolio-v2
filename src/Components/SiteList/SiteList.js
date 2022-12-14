6a04c7786d227f21d8fe5ba05cab7ae54984dafb
chris3evans <chris3evans.ce@gmail.com>
2022-12-14 08:46:07 +0000

    Added specter and ross site card to portfolio

--- src/Components/SiteList/SiteList.js
+++ src/Components/SiteList/SiteList.js
@@ -1,11 +1,13 @@
 import styles from "./SiteList.module.css";
 
 import AceCam from "../SiteItems/AceCam";
+import SpecterAndRoss from "../SiteItems/SpecterAndRoss";
 
 const SiteList = function () {
   return (
     <ul className={styles["site-list"]}>
       <AceCam></AceCam>
+      <SpecterAndRoss></SpecterAndRoss>
     </ul>
   );
 };

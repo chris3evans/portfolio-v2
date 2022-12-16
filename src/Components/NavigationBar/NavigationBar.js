c4b638c8568f8c577b54fb6342f27dbdea9c9ef5
chris3evans <chris3evans.ce@gmail.com>
2022-12-16 12:37:54 +0000

    Built a responsive mobile navigation

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -1,12 +1,36 @@
 import styles from "./NavigationBar.module.css";
 import PageSectionList from "../PageSectionList/PageSectionList";
 import LinkList from "../LinksList/LinksList";
+import "../../index.css";
 
 const NavigationBar = function () {
+  const navigation = document.querySelector(".navigation");
+
+  const openMobileNavigationHandler = function () {
+    navigation.classList.add("navigation-open");
+  };
+  const closeMobileNavigationHandler = function () {
+    navigation.classList.remove("navigation-open");
+  };
+
   return (
-    <nav className={styles["navigation"]}>
+    <nav className="navigation">
       <LinkList></LinkList>
       <PageSectionList></PageSectionList>
+      <button className={styles["mobile-navigation"]}>
+        <img
+          onClick={openMobileNavigationHandler}
+          id="open-icon"
+          className="mobile-navigation-icon"
+          src={require("../../assets/menu-icon.png")}
+        ></img>
+        <img
+          onClick={closeMobileNavigationHandler}
+          id="close-icon"
+          className="mobile-navigation-icon"
+          src={require("../../assets/close-icon.png")}
+        ></img>
+      </button>
     </nav>
   );
 };

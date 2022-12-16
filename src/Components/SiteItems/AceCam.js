5613046fc8b5449902dac4cb3f4a362602925c39
chris3evans <chris3evans.ce@gmail.com>
2022-12-16 08:59:29 +0000

    Made entire of work card clickable link

--- src/Components/SiteItems/AceCam.js
+++ src/Components/SiteItems/AceCam.js
@@ -4,40 +4,46 @@ import "../../index.css";
 const AceCam = function () {
   return (
     <li key="acecam" className="card-site">
-      <div className="card-front">
-        <img
-          className="project-image"
-          src={require("../../assets/acecam.png")}
-        ></img>
-        <h3 className={styles["project-title"]}>AceCam</h3>
-      </div>
-      <div className="card-back">
-        <h3 className="tech-title">Tech Stack</h3>
-        <div className="tech-list">
-          <h4 className="tech-type-title">Front End</h4>
-          <ul className={styles["front-end-list"]}>
-            <li key="HTML5">
-              <p className="tech-text">HTML5</p>
-            </li>
-            <li key="CSS3">
-              <p className="tech-text">CSS3</p>
-            </li>
-            <li key="SASS">
-              <p className="tech-text">SASS/SCSS</p>
-            </li>
-          </ul>
+      <a
+        href="https://ace-cam.netlify.app"
+        target="_blank"
+        className="card-link"
+      >
+        <div className="card-front">
+          <img
+            className="project-image"
+            src={require("../../assets/acecam.png")}
+          ></img>
+          <h3 className={styles["project-title"]}>AceCam</h3>
         </div>
-        <div className="button-container">
-          <a
-            type="button"
-            href="https://ace-cam.netlify.app"
-            target="_blank"
-            className="visit-button"
-          >
-            Visit
-          </a>
+        <div className="card-back">
+          <h3 className="tech-title">Tech Stack</h3>
+          <div className="tech-list">
+            <h4 className="tech-type-title">Front End</h4>
+            <ul className={styles["front-end-list"]}>
+              <li key="HTML5">
+                <p className="tech-text">HTML5</p>
+              </li>
+              <li key="CSS3">
+                <p className="tech-text">CSS3</p>
+              </li>
+              <li key="SASS">
+                <p className="tech-text">SASS/SCSS</p>
+              </li>
+            </ul>
+          </div>
+          <div className="button-container">
+            <a
+              type="button"
+              href="https://ace-cam.netlify.app"
+              target="_blank"
+              className="visit-button"
+            >
+              Visit
+            </a>
+          </div>
         </div>
-      </div>
+      </a>
     </li>
   );
 };
--- src/Components/SiteItems/SpecterAndRoss.js
+++ src/Components/SiteItems/SpecterAndRoss.js
@@ -4,41 +4,47 @@ import "../../index.css";
 const SpecterAndRoss = function () {
   return (
     <li key="specter-ross" className="card-site">
-      <div className="card-front">
-        <img
-          className="project-image"
-          src={require("../../assets/specter-ross.png")}
-        ></img>
-        <h3 className={styles["project-title-top"]}>Specter &</h3>
-        <h3 className={styles["project-title-bottom"]}>Ross</h3>
-      </div>
-      <div className="card-back">
-        <h3 className="tech-title">Tech Stack</h3>
-        <div className="tech-list">
-          <h4 className="tech-type-title">Front End</h4>
-          <ul className={styles["front-end-list"]}>
-            <li key="HTML5">
-              <p className="tech-text">HTML5</p>
-            </li>
-            <li key="CSS3">
-              <p className="tech-text">CSS3</p>
-            </li>
-            <li key="SASS">
-              <p className="tech-text">SASS/SCSS</p>
-            </li>
-          </ul>
+      <a
+        href="https://specter-and-ross.netlify.app"
+        target="_blank"
+        className="card-link"
+      >
+        <div className="card-front">
+          <img
+            className="project-image"
+            src={require("../../assets/specter-ross.png")}
+          ></img>
+          <h3 className={styles["project-title-top"]}>Specter &</h3>
+          <h3 className={styles["project-title-bottom"]}>Ross</h3>
         </div>
-        <div className="button-container">
-          <a
-            type="button"
-            href="https://specter-and-ross.netlify.app"
-            target="_blank"
-            className="visit-button"
-          >
-            Visit
-          </a>
+        <div className="card-back">
+          <h3 className="tech-title">Tech Stack</h3>
+          <div className="tech-list">
+            <h4 className="tech-type-title">Front End</h4>
+            <ul className={styles["front-end-list"]}>
+              <li key="HTML5">
+                <p className="tech-text">HTML5</p>
+              </li>
+              <li key="CSS3">
+                <p className="tech-text">CSS3</p>
+              </li>
+              <li key="SASS">
+                <p className="tech-text">SASS/SCSS</p>
+              </li>
+            </ul>
+          </div>
+          <div className="button-container">
+            <a
+              type="button"
+              href="https://specter-and-ross.netlify.app"
+              target="_blank"
+              className="visit-button"
+            >
+              Visit
+            </a>
+          </div>
         </div>
-      </div>
+      </a>
     </li>
   );
 };
